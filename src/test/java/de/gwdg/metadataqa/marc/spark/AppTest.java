acd61d89ec6681bac99850fde913ba44756328bf
Péter Király <pkiraly@gwdg.de>
2025-05-07 14:16:51 +0200

    Move Spark related code from QA Catalogue #1

---
+++ src/test/java/de/gwdg/metadataqa/marc/spark/AppTest.java
@@ -0,0 +1,19 @@
+package de.gwdg.metadataqa.marc.spark;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
